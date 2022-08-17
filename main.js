d38f26d1a8425502e744855a7ec7152b07bae145
yousra Jaradh <yousrakhaleel@gmail.com>
2022-08-17 16:08:08 +0300

    add style and js

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-let allQuestions = document.querySelectorAll(".question");
-let Faq = (e) => {
-    let answer = e.currentTarget;
-    console.log(e)
+let questions = document.querySelectorAll(".question");
 
+for (let i = 0; i < questions.length; i++) {
+  let answer = questions[i].nextElementSibling;
+  answer.style.display = "none";
+  questions[i].addEventListener("click", (e) => {
+    let answer = e.currentTarget.nextElementSibling;
+    let question = e.currentTarget.children[0];
+    let arrowIcon = e.currentTarget.children[1];
+    if (answer.style.display === "none") {
+      answer.style.display = "block";
+      question.style.fontWeight = "700";
+      arrowIcon.style.transform = "rotate(180deg)";
+    } else {
+      answer.style.display = "none";
+      question.style.fontWeight = "400";
+      arrowIcon.style.transform = "rotate(0deg)";
+    }
+  });
 }
